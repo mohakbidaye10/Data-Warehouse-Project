7ba30f4d022389d665a75a8d7da7ccdc6d54bc1d
mohakbidaye10 <108085582+mohakbidaye10@users.noreply.github.com>
2025-09-20 12:01:39 +0530

    Create quality_checks_gold.sql

---
+++ Tests/quality_checks_gold.sql
@@ -0,0 +1,60 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+*/
+
+-- Quality Check of gold table
+
+SELECT * FROM gold.dim_customers
+
+SELECT DISTINCT gender 
+FROM gold.dim_customers
+
+SELECT * FROM gold.dim_products
+
+SELECT * FROM gold.fact_sales
+
+-- Checking foreign key integrity 
+-- Check the data model connectivity between fact and dimensions for gold.fact_sales
+
+SELECT *  
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+WHERE p.product_key IS NULL
+
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- Check for duplicates of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+
+
+
+
+
+
+
+
